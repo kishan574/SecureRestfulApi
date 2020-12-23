770cad4b73a59c1aa5a1d515311862aeb2afece5
kishan sharma <kishu02@hotmail.com>
2020-12-23 21:47:41 +0545

    changed the enum weather forecast

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -13,9 +13,8 @@ namespace CoreWebApiRestArchi_Pluralsight.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Smoking", "Sweltering", "Scorching"
         };
-
         private readonly ILogger<WeatherForecastController> _logger;
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
